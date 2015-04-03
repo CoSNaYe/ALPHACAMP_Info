9f2dab7b4c7726614129cb6deea1de6ff42111df
陳逸仁 <IanCoSNaYe@chinitsujins-MacBook-Pro.local>
2015-04-03 23:49:52 +0800

    modify navigation bar image of personal info landing page

--- PersonLandingPage.m
+++ PersonLandingPage.m
@@ -9,26 +9,45 @@
 #import "PersonLandingPage.h"
 #import "PersonalInfoVC.h"
 
+@interface PersonLandingPage()
+@property (weak, nonatomic) IBOutlet UIButton *toPersonDetailButton;
+@property (strong, nonatomic) UIImageView *navigationTitleImageView;
+@end
 @implementation PersonLandingPage
 
 - (void)viewDidLoad
 {
-    
-}
-- (IBAction)toPersonDetailButton:(UIButton *)sender {
-    
-    PersonalInfoVC *pIVC = [[PersonalInfoVC alloc] init];
-    
-    [self presentViewController: pIVC animated:YES completion:nil];
+    NSLog(@"person landing page load");
+    [self setupUI];
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+- (void)setupUI
 {
-    if ([segue.destinationViewController isKindOfClass:[PersonalInfoVC class]]) {
-        if ( [segue.identifier isEqualToString:@"toPersonalDetail"] ){
-            
-        }
+    //set the push button
+    self.toPersonDetailButton.layer.masksToBounds = YES;
+    self.toPersonDetailButton.layer.cornerRadius = 3.0;
+    
+    //set the navigation header image
+    self.navigationItem.titleView = self.navigationTitleImageView;
+    self.navigationController.navigationBar.backgroundColor = [UIColor grayColor];
+}
+
+- (UIImageView *)navigationTitleImageView
+{
+    if (!_navigationTitleImageView) {
+        UIImage *myImage = [UIImage imageNamed:@"navigationImage"];
+        UIImageView *titleImage = [[UIImageView alloc]
+                                   initWithFrame:CGRectMake(self.view.frame.size.width/3, 50, self.view.frame.size.width/3, 20)];
+        
+        titleImage.image = myImage;
+        titleImage.contentMode = UIViewContentModeScaleAspectFit;
+        
+        _navigationTitleImageView = titleImage;
     }
+    return _navigationTitleImageView;
+    
+    //another way to set navigation bar image
+    //self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"navigationImage"]];
 }
 
 @end
