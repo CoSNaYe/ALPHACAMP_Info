e7b196e87513e4b2f9ead425b40d1f01cab54910
CoSNaYe <CoSNaYe@CoSNaYetekiiMac.local>
2015-04-03 21:41:43 +0800

    add Personal info landing page

---
+++ PersonLandingPage.h
@@ -0,0 +1,13 @@
+//
+//  PersonLandingPage.h
+//  ALPHACamp
+//
+//  Created by CoSNaYe on 4/3/15.
+//  Copyright (c) 2015 ALPHACamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonLandingPage : UIViewController
+
+@end
---
+++ PersonLandingPage.m
@@ -0,0 +1,13 @@
+//
+//  PersonLandingPage.m
+//  ALPHACamp
+//
+//  Created by CoSNaYe on 4/3/15.
+//  Copyright (c) 2015 ALPHACamp. All rights reserved.
+//
+
+#import "PersonLandingPage.h"
+
+@implementation PersonLandingPage
+
+@end
