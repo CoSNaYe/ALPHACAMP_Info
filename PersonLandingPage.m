32b2ccad81d57a190c64ccd10f36dd681a19cf3e
CoSNaYe <CoSNaYe@CoSNaYetekiiMac.local>
2015-04-03 22:02:54 +0800

    add push button for personal Landing page

--- PersonLandingPage.m
+++ PersonLandingPage.m
@@ -7,7 +7,28 @@
 //
 
 #import "PersonLandingPage.h"
+#import "PersonalInfoVC.h"
 
 @implementation PersonLandingPage
+
+- (void)viewDidLoad
+{
+    
+}
+- (IBAction)toPersonDetailButton:(UIButton *)sender {
+    
+    PersonalInfoVC *pIVC = [[PersonalInfoVC alloc] init];
+    
+    [self presentViewController: pIVC animated:YES completion:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.destinationViewController isKindOfClass:[PersonalInfoVC class]]) {
+        if ( [segue.identifier isEqualToString:@"toPersonalDetail"] ){
+            
+        }
+    }
+}
 
 @end
